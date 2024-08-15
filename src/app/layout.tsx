a92c5a536885775fbab8e24cdd6e69edae6c3e89
21120447 <haonhat2729@gmail.com>
2024-08-15 16:36:40 +0700

    :ambulance: fix: hotfix not found page (root)

---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "EduB - Choose your course, master your future",
+  description: "Choose your course, master your future",
+  icons: "logo.svg",
+};
+
+export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return <React.Fragment>{children}</React.Fragment>;
+}
