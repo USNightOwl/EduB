2e33bd4ce7430984449e69f50bc9a66a11df6eb6
21120447 <haonhat2729@gmail.com>
2024-08-18 14:22:14 +0700

    :hammer: refactor: padding mobile form

--- src/ui/auth/form/register-form.tsx
+++ src/ui/auth/form/register-form.tsx
@@ -11,7 +11,7 @@ const RegisterForm = () => {
   const t = useTranslations();
 
   return (
-    <div className="flex w-[60%] max-md:w-full max-lg:w-[80%] mx-auto py-5 max-sm:py-3">
+    <div className="flex w-[60%] max-md:w-full max-lg:w-[80%] mx-auto py-5 max-sm:py-2">
       <Paper elevation={5} className="p-4 w-full" sx={{ bgcolor: "box.bg", color: "box.text" }}>
         <div className="flex items-center flex-col justify-center">
           <Typography variant="h4" className="uppercase">
@@ -26,7 +26,7 @@ const RegisterForm = () => {
             <Stack direction="column" className="w-full" spacing={1} mt={1}>
               <Stack spacing={1} direction="row">
                 <Typography>{t("Auth.Form.have-account")}</Typography>
-                <Link href={"/auth/register"} className="text-blue-700 hover:underline">
+                <Link href={"/auth/login"} className="text-blue-700 hover:underline">
                   {t("Header.login") + " " + t("Global.now")}
                 </Link>
               </Stack>
