58157302f2a440fea1bf2e87e9bfc760719f7ec3
21120447 <haonhat2729@gmail.com>
2024-08-20 00:14:43 +0700

    :hammer: refactor: open user menu by click

--- src/ui/header/user-menu.tsx
+++ src/ui/header/user-menu.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { type Session } from "next-auth";
 import React from "react";
 import { styled } from "@mui/material/styles";
@@ -7,13 +8,21 @@ import { useTranslations } from "next-intl";
 import LogoutIcon from "@mui/icons-material/Logout";
 import Image from "next/image";
 import { signOut } from "next-auth/react";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
 
 interface Props {
   auth: Session;
 }
 
 const HtmlTooltip = styled(({ className, ...props }: TooltipProps) => (
-  <Tooltip leaveDelay={200} {...props} classes={{ popper: className }} />
+  <Tooltip
+    PopperProps={{
+      disablePortal: true,
+    }}
+    leaveDelay={200}
+    {...props}
+    classes={{ popper: className }}
+  />
 ))(({ theme }) => ({
   [`& .${tooltipClasses.tooltip}`]: {
     backgroundColor: "#f5f5f9",
@@ -26,24 +35,45 @@ const HtmlTooltip = styled(({ className, ...props }: TooltipProps) => (
 
 const UserMenu = ({ auth }: Props) => {
   const t = useTranslations("Header");
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
   return (
-    <HtmlTooltip
-      title={
-        <div className="flex items-center flex-col py-1">
-          {auth?.user?.image && (
-            <Image src={auth?.user?.image} alt={"avatar"} height={60} width={60} className="rounded-full" />
-          )}
-          <Typography className="text-base font-bold">{auth?.user?.name}</Typography>
-          <Typography className="text-base text-slate-400 mt-1">{auth?.user?.email}</Typography>
-          <div className="w-full border-b py-1 mb-2"></div>
-          <Button startIcon={<LogoutIcon />} color="error" onClick={() => signOut()}>
-            {t("logout")}
+    <ClickAwayListener onClickAway={handleClose}>
+      <div>
+        <HtmlTooltip
+          open={open}
+          onClose={handleClose}
+          disableFocusListener
+          disableHoverListener
+          disableTouchListener
+          title={
+            <div className="flex items-center flex-col py-1">
+              {auth?.user?.image && (
+                <Image src={auth?.user?.image} alt={"avatar"} height={60} width={60} className="rounded-full" />
+              )}
+              <Typography className="text-base font-bold">{auth?.user?.name}</Typography>
+              <Typography className="text-base text-slate-400 mt-1">{auth?.user?.email}</Typography>
+              <div className="w-full border-b py-1 mb-2"></div>
+              <Button startIcon={<LogoutIcon />} color="error" onClick={() => signOut()}>
+                {t("logout")}
+              </Button>
+            </div>
+          }
+        >
+          <Button onClick={handleOpen} className="font-semibold">
+            {auth?.user?.name}
           </Button>
-        </div>
-      }
-    >
-      <Button className="font-semibold">{auth?.user?.name}</Button>
-    </HtmlTooltip>
+        </HtmlTooltip>
+      </div>
+    </ClickAwayListener>
   );
 };
 
